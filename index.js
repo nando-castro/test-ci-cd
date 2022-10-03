996f348116f335e6e03a0915b2066474111a28d5
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-03 19:47:49 -0300

    iniciando a bagaceira

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Xablau!!!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Servidor rodou suave na porta: ${PORT}`));
